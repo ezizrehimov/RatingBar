547b7477e71c14510bbb2fd7bf7f145656ea9dfc
Eziz Rehimov <fantastig@bk.ru>
2021-06-08 16:39:56 +0400

    Completed

---
+++ app/src/main/java/com/eziz/ratingbar/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.eziz.ratingbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ratingBar.setOnRatingBarChangeListener { ratingBar, rating, fromUser ->
+            textView.text = rating.toString()
+        }
+        button.setOnClickListener {
+            textView2.text = ratingBar.rating.toString()
+        }
+    }
+}
